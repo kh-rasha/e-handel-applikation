da4f5833372c0091efdb2a8bceb02692aff1a481
kh-rasha <rasha.alkhateeb97@gmail.com>
2025-12-16 11:51:28 +0100

    Initial Spring Boot setup

---
+++ src/main/java/se/yh/ehandel/ehandelapplikation/EHandelApplikationApplication.java
@@ -0,0 +1,13 @@
+package se.yh.ehandel.ehandelapplikation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EHandelApplikationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EHandelApplikationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/yh/ehandel/ehandelapplikation/EHandelApplikationApplicationTests.java
@@ -0,0 +1,13 @@
+package se.yh.ehandel.ehandelapplikation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EHandelApplikationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
