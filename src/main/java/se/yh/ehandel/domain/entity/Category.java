54e143bf002296987e62b22b4ecb4b56510ca36b
kh-rasha <rasha.alkhateeb97@gmail.com>
2025-12-16 20:13:16 +0100

    commitat application.properties , class Category.java

--- src/main/java/se/yh/ehandel/domain/entity/Category.java
+++ src/main/java/se/yh/ehandel/domain/entity/Category.java
@@ -1,4 +1,19 @@
 package se.yh.ehandel.domain.entity;
 
+import jakarta.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "category")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products;
 }
