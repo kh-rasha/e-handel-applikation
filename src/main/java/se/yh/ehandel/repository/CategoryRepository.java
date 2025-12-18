3ae62a405bbdb8c2bc86b93fe6845855900b7544
kh-rasha <rasha.alkhateeb97@gmail.com>
2025-12-18 09:23:59 +0100

    commit class CategoryRepository.java , ProductRepository.java

--- src/main/java/se/yh/ehandel/repository/CategoryRepository.java
+++ src/main/java/se/yh/ehandel/repository/CategoryRepository.java
@@ -1,4 +1,10 @@
 package se.yh.ehandel.repository;
 
-public class CategoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.yh.ehandel.domain.entity.Category;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByNameIgnoreCase(String name);
 }
--- src/main/java/se/yh/ehandel/repository/ProductRepository.java
+++ src/main/java/se/yh/ehandel/repository/ProductRepository.java
@@ -1,4 +1,12 @@
 package se.yh.ehandel.repository;
 
-public class ProductRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import se.yh.ehandel.domain.entity.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findBySku(String sku);
+    List<Product> findByNameContainingIgnoreCase(String q);
 }
