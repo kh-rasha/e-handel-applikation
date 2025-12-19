afc3f2dd5a27ce6bb590c3ef31192b44aeb3a6d9
Daniel B <158176937+Mrdbs95@users.noreply.github.com>
2025-12-19 13:07:24 +0100

    Skapa categoryCommand klasser

---
+++ src/main/java/se/yh/ehandel/cli/command/CategoryAddCommand.java
@@ -0,0 +1,31 @@
+package se.yh.ehandel.cli.command;
+
+import se.yh.ehandel.cli.ConsoleIO;
+import se.yh.ehandel.service.CategoryService;
+
+public class CategoryAddCommand implements Command {
+    private final CategoryService categoryService;
+    private final ConsoleIO io;
+
+    public CategoryAddCommand(CategoryService categoryService, ConsoleIO io) {
+        this.categoryService = categoryService;
+        this.io = io;
+    }
+
+    @Override
+    public String name() {
+        return "category add";
+    }
+
+    @Override
+    public String help() {
+        return "category add - create new category";
+    }
+
+    @Override
+    public void execute() {
+        String name = io.readLine("Category name: ");
+        categoryService.create(name);
+        io.println("Category created: " + name);
+    }
+}
