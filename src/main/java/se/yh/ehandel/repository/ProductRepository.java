3ca5329bb5f7dbe2d81774a1807fede305be30fa
alexandersson98 <alexandersson98l@gmail.com>
2025-12-22 18:21:15 +0100

    product test

--- src/main/java/se/yh/ehandel/repository/ProductRepository.java
+++ src/main/java/se/yh/ehandel/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package se.yh.ehandel.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import se.yh.ehandel.domain.entity.Product;
 
 import java.util.List;
